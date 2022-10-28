808840c5679593cbd950f48a3c5752433893b553
Paul Hammersley <paulhammersley03@gmail.com>
2022-10-28 18:50:09 +0100

    Tidying up

--- new_linux_install_setup.sh
+++ new_linux_install_setup.sh
@@ -11,8 +11,8 @@ WriteToConsole () {
 	echo
 }
 
-##Install Curl
-sudo apt install apt-transport-https curl
+##Install Dependencies
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
 ##Add the repos
 	WriteToConsole "Getting the repo's..."
@@ -34,7 +34,6 @@ sudo apt install apt-transport-https curl
 	wget https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	##Docker
-	sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian buster stable" | sudo tee /etc/apt/sources.list.d/docker.list
 
